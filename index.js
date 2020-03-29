94049f9197be2254474546856b75601061feb3d5
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-03-29 14:57:48 -0300

    ok

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const db = require('./config/db')
+const consign = require('consign')
 
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
+
+app.db = db
 
 app.get('/', (req, res) => {
     res.status(200).send('Meu Backend')
